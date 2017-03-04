e942e7fe1b12a68c3dab73d14bd5589e0436a0d9
Carlos Cortes <carlos.cortes@prodigious.com>
2017-03-04 11:28:49 -0500

    pasa a 15

--- hola.java
+++ hola.java
@@ -1,6 +1,6 @@
 public class hola {
   public static void main(String[] args){
-    for(int i=1;i<=10;i++){
+    for(int i=1;i<=15;i++){
       System.out.println("Hola a todos ... "+i);
     }
   }
